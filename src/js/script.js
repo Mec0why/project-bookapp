f38632270c611b452f481cd2c93d0ed2a97ed1ca
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-03-13 20:12:46 +0100

    Add JS script Variables + app.init() + Book constructor

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,82 @@
-// This is to try
+/* global Handlebars, utils, dataSource */ // eslint-disable-line
+
+{
+  ('use strict');
+
+  const select = {
+    templateOf: {
+      book: '#template-book',
+    },
+    book: {
+      header: '.book__header',
+      coverImage: '.book__image',
+      ratings: '.book__rating',
+    },
+    containerOf: {
+      book: '.books-list',
+    },
+  };
+
+  const settings = {
+    amountRating: {
+      defaultValue: 1,
+    },
+  };
+
+  const templates = {
+    book: Handlebars.compile(
+      document.querySelector(select.templateOf.book).innerHTML
+    ),
+  };
+
+  class Book {
+    constructor(id, data) {
+      const thisBook = this;
+
+      thisBook.id = id;
+      thisBook.data = data;
+
+      thisBook.renderInMenu();
+    }
+
+    renderInMenu() {
+      const thisBook = this;
+
+      const generatedHTML = templates.book(thisBook.data);
+
+      thisBook.element = utils.createDOMFromHTML(generatedHTML);
+
+      const bookContainer = document.querySelector(select.containerOf.book);
+
+      bookContainer.appendChild(thisBook.element);
+    }
+  }
+
+  const app = {
+    initBook: function () {
+      const thisApp = this;
+
+      for (let bookData in thisApp.data.books) {
+        new Book(thisApp.data.books[bookData].id, thisApp.data.books[bookData]);
+      }
+    },
+
+    initData: function () {
+      const thisApp = this;
+      thisApp.data = dataSource;
+      thisApp.initBook();
+    },
+
+    init: function () {
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+
+      thisApp.initData();
+    },
+  };
+
+  app.init();
+}
